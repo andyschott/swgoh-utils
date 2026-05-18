cde59ead22e2acfce4fe41bc3a4bae4a47d6ea4b
Andy Schott <andy@andyschott.com>
2026-05-18 07:57:11 -0400

    Update Signal Data Drop Rates

--- src/app/signalData/signal-data-farming.ts
+++ src/app/signalData/signal-data-farming.ts
@@ -11,12 +11,12 @@ const Fragemented8C = 1.41;
 const Incomplete8F = 0.91;
 const Flawed8G = 0.61;
 
-const Fragmented9B = 1.2710;
-const Fragmented9D = 1.2870;
-const Incomplete9B = 0.5838;
-const Incomplete9F = 0.8497;
-const Flawed9D = 0.4135;
-const Flawed9F = 0.4999;
+const Fragmented9B = 1.2628;
+const Fragmented9D = 1.2907;
+const Incomplete9B = 0.6034;
+const Incomplete9F = 0.8685;
+const Flawed9D = 0.4132;
+const Flawed9F = 0.4992;
 
 const Tier8SimsPerDay = 32;
 const Tier9SimsPerDay = 26;
