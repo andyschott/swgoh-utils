1bbee3ce8c637ea7efaa9227a2d76603643d7aae
Andy Schott <andy@andyschott.com>
2026-05-21 08:15:56 -0400

    setup TypeGen

---
+++ api/SwgohApi.Models.TypeGen/SwgohApiGenerationSpec.cs
@@ -0,0 +1,19 @@
+﻿using TypeGen.Core.SpecGeneration;
+
+namespace SwgohApi.Models.TypeGen;
+
+public class SwgohApiGenerationSpec : GenerationSpec
+{
+  public override void OnBeforeGeneration(OnBeforeGenerationArgs args)
+  {
+    var assembly = typeof(SwgohApi.Models.Users.UserDto).Assembly;
+    var types = assembly.GetTypes();
+
+    foreach (var type in types)
+    {
+      AddInterface(type);
+    }
+
+    base.OnBeforeGeneration(args);
+  }
+}
