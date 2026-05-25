3a50456b87b62dddb0d4467edb835fffd6fd4830
Andy Schott <andy@andyschott.com>
2026-05-25 13:29:07 -0400

    remove unused methods

--- api/SwgohApi.Infrastructure/Postgres/PostgresCharacterRepository.cs
+++ api/SwgohApi.Infrastructure/Postgres/PostgresCharacterRepository.cs
@@ -24,23 +24,4 @@ public class PostgresCharacterRepository : PostgresEarnableRepository<Character>
         character.IsAccelerated = isAccelerated;
       });
   }
-
-  public async Task<IEnumerable<Character>> GetCharactersForUser(User user)
-  {
-    // TODO: Re-implement once database is settled
-    return [];
-    // return await _dbContext.Characters
-    //   .Include(c => c.EarnableShards)
-    //   .Where(c => c.EarnableShards == null || c.EarnableShards.UserId == user.Id)
-    //   .ToListAsync();
-  }
-
-  public async Task<Character?> GetCharacterForUser(string id, User user)
-  {
-    // TODO: Re-implement once database is settled
-    return null;
-    // return await _dbContext.Characters
-    //   .Include(c => c.EarnableShards)
-    //   .FirstOrDefaultAsync(c => c.Id == id);
-  }
 }
