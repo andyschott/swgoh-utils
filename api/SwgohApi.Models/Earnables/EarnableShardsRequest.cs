611bec762adbc405d204f117824faf0c321230fc
Andy Schott <andy@andyschott.com>
2026-05-24 20:50:53 -0400

    validate update earnable shards for user endpoint

--- api/SwgohApi.Models/Earnables/EarnableShardsRequest.cs
+++ api/SwgohApi.Models/Earnables/EarnableShardsRequest.cs
@@ -1,4 +1,7 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace SwgohApi.Models.Earnables;
 
-public record EarnableShardsRequest(int Shards,
+public record EarnableShardsRequest(
+  [property: Range(0, 330)]int Shards,
   FarmingStatus FarmingStatus);
