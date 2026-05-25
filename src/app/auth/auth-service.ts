8211e96d23d705455efddffd2a889c567ce1ad36
Andy Schott <andy@andyschott.com>
2026-05-25 17:16:53 -0400

    auto hide the login form once logged in

--- src/app/auth/auth-service.ts
+++ src/app/auth/auth-service.ts
@@ -1,8 +1,9 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { TokenResponse } from '../apiModels/token-response';
 import { LoginRequest } from '../apiModels/login-request';
+import { map, Observable } from 'rxjs';
 
 const AuthKey = "authentication";
 
@@ -18,9 +19,8 @@ export class AuthService {
   private readonly httpClient = inject(HttpClient);
   private readonly authUrl = `${environment.apiBaseUrl}/auth`;
 
-  isLoggedIn() {
-    return localStorage.getItem(AuthKey) !== null;
-  }
+  private readonly _isLoggedIn = signal(this.hasSavedToken());
+  readonly isLoggedIn = this._isLoggedIn.asReadonly();
 
   login(username: string, password: string) {
     const request: LoginRequest = {
@@ -28,16 +28,21 @@ export class AuthService {
       password
     };
     this.httpClient.post<TokenResponse>(`${this.authUrl}/login`, request)
-    .subscribe((response) => {
-      const auth: Token = {
-        accessToken: response.accessToken,
-        refreshToken: response.refreshToken
-      };
-      this.saveToken(auth);
-    });
+      .subscribe((response) => {
+        const auth: Token = {
+          accessToken: response.accessToken,
+          refreshToken: response.refreshToken
+        };
+        this.saveToken(auth);
+        this._isLoggedIn.set(true);
+      });
   }
 
   private saveToken(auth: Token) {
     localStorage.setItem(AuthKey, JSON.stringify(auth));
+  }
+
+  private hasSavedToken() {
+    return localStorage.getItem(AuthKey) !== null;
   }
 }
