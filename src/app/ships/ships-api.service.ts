a5c7b0e2c35d39384dad248bb546ab6aa9df4405
Andy Schott <andy@andyschott.com>
2026-05-25 20:39:58 -0400

    add methods to get a user's characters and ships

--- src/app/ships/ships-api.service.ts
+++ src/app/ships/ships-api.service.ts
@@ -1,8 +1,9 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpContext } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
 import { Ship } from '../apiModels/ship';
+import { AUTHENTICATED_REQUEST } from '../auth/auth-interceptor';
 
 @Injectable({
   providedIn: 'root',
@@ -13,5 +14,11 @@ export class ShipsApiService {
 
   public getShips(): Observable<Ship[]> {
     return this.httpClient.get<Ship[]>(this.shipsUrl);
+  }
+
+  public getShipsForUser(): Observable<Ship[]> {
+    return this.httpClient.get<Ship[]>(`${environment.apiBaseUrl}/shipssForUser`, {
+      context: new HttpContext().set(AUTHENTICATED_REQUEST, true)
+    });
   }
 }
