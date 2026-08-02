f90316310904f3f7e4bc1a5d3645e4c33286188a
Andy Schott <andy@andyschott.com>
2026-08-02 15:17:21 -0400

    load characters from the database

--- api/SwgohApi/Controllers/CharactersController.cs
+++ api/SwgohApi/Controllers/CharactersController.cs
@@ -1,11 +1,32 @@
 using Microsoft.AspNetCore.Mvc;
+using SwgohApi.Infrastructure;
+using SwgohApi.Mapping;
+using SwgohApi.Models;
+using SwgohApi.Models.Earnables;
+using InternalCharacter = SwgohApi.Infrastructure.Models.Character;
 
 namespace SwgohApi.Controllers;
 
 public class CharactersController : Controller
 {
-  public IActionResult Index()
+  private readonly IEarnableRepository<InternalCharacter> _characterRepository;
+  private readonly IMapper<InternalCharacter, Character> _mapper;
+
+  public CharactersController(IEarnableRepository<InternalCharacter> characterRepository,
+    IMapper<InternalCharacter, Character> mapper)
   {
-    return View();
+    _characterRepository = characterRepository;
+    _mapper = mapper;
+  }
+
+  public async Task<IActionResult> Index()
+  {
+    var internalCharacters = await _characterRepository.GetEarnables();
+    var characters = internalCharacters.Select(_mapper.MapTo);
+    var model = new CharactersViewModel
+    {
+      Characters = [.. characters]
+    };
+    return View(model);
   }
 }
---
+++ api/SwgohApi/Models/CharactersViewModel.cs
@@ -0,0 +1,8 @@
+using SwgohApi.Models.Earnables;
+
+namespace SwgohApi.Models;
+
+public class CharactersViewModel
+{
+  public required IEnumerable<Character> Characters { get; set; }
+}
