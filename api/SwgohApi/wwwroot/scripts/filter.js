269c59c7a33204665c172765be7282bb1365dd79
Andy Schott <andy@andyschott.com>
2026-08-08 11:48:31 -0400

    simplify search algorithm

--- api/SwgohApi/wwwroot/scripts/filter.js
+++ api/SwgohApi/wwwroot/scripts/filter.js
@@ -1,37 +1,20 @@
-const items = [];
-
 function onFilter(event) {
   const searchTerm = event.target.value;
+  const rows = document.querySelectorAll('#data-table tbody tr');
   if (searchTerm.length === 0) {
-    const rows = getDataTableRows();
     for (let index = 0; index < rows.length; index++) {
       rows[index].style.display = '';
     }
     return;
   }
 
-  for (let index = 0; index < items.length; index++) {
-    const item = items[index];
-    const match = item.name.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase());
-    item.row.style.display = match ? '' : 'none';
+  for (let index = 0; index < rows.length; index++) {
+    const row = rows[index];
+    const th = row.cells[0];
+    const match = th.innerText.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase());
+    rows[index].style.display = match ? '' : 'none';
   }
-}
-
-function getDataTableRows() {
-  return document.querySelectorAll('#data-table tbody tr');
 }
 
 document.getElementById('filter')
   .addEventListener("input", onFilter);
-
-document.addEventListener('DOMContentLoaded', () => {
-  const rows = getDataTableRows();
-  for (let index = 0; index < rows.length; index++) {
-    const row = rows[index];
-    const th = row.cells[0];
-    items.push({
-      name: th.innerText,
-      row: row
-    });
-  }
-});
