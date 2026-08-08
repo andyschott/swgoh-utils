ed2423f9bb7104c1d52fd4d735cb904ee058687e
Andy Schott <andy@andyschott.com>
2026-08-08 10:34:21 -0400

    add search field for characters and ships

---
+++ api/SwgohApi/wwwroot/scripts/filter.js
@@ -0,0 +1,37 @@
+const items = [];
+
+function onFilter(event) {
+  const searchTerm = event.target.value;
+  if (searchTerm.length === 0) {
+    const rows = getDataTableRows();
+    for (let index = 0; index < rows.length; index++) {
+      rows[index].style.visibility = 'visible';
+    }
+    return;
+  }
+
+  for (let index = 0; index < items.length; index++) {
+    const item = items[index];
+    const match = item.name.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase());
+    item.row.style.visibility = match ? 'visible' : 'collapse';
+  }
+}
+
+function getDataTableRows() {
+  return document.querySelectorAll('#data-table tbody tr');
+}
+
+document.getElementById('filter')
+  .addEventListener("input", onFilter);
+
+document.addEventListener('DOMContentLoaded', () => {
+  const rows = getDataTableRows();
+  for (let index = 0; index < rows.length; index++) {
+    const row = rows[index];
+    const th = row.cells[0];
+    items.push({
+      name: th.innerText,
+      row: row
+    });
+  }
+});
